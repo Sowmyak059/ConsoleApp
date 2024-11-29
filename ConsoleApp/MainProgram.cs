d6c929397ae5a8a08f83f49c0e66e64efb9c66da
sowmyak059 <sowmyak059@gmail.com>
2024-11-29 13:39:15 +0100

    Added refactored code and w48 project

---
+++ ConsoleApp/MainProgram.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Assignment
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please choose which assignment want to run");
+        Console.WriteLine("1 - Assignment W47");
+        Console.Write("2 - Assignment W48");
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine(" \"(Current week assignment)\"");
+        Console.ResetColor();
+        
+        Console.Write("Enter number : "); 
+        String strInputValue = Console.ReadLine().Trim();
+
+        int iInput = ((strInputValue.Length > 0) && (strInputValue.All(char.IsDigit))) ? Convert.ToInt16(strInputValue.Trim()) : 0;
+        switch(iInput){
+            case 1:
+            {
+                Assignment47 assignement47 = new Assignment47();
+                assignement47.w47tAssignment();
+                break;
+            }
+            default:
+            {
+                Assignment48 assignement48 = new Assignment48();
+                assignement48.w48Assignment();
+                break;
+            }
+        }
+    }
+  }
+}
